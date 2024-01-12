2626a774696ddb8c83b03dc209dde996efb5a922
wsdula <wsdula@gmail.com>
2024-01-11 20:24:22 -0500

    moved ui stuff to separate file for dev

---
+++ ui.py
@@ -0,0 +1,15 @@
+# This application is a quiz game, where the user can choose a category and answer the questions.
+# Each question has 1 correct answer.
+# Each question is worth 10 points
+
+# Importing the necessary modules
+import tkinter as tk
+# import utils
+
+window = tk.Tk()
+greeting = tk.Label(text="Welcome to the Quiz Game!")
+greeting.pack()
+
+window.title("Quiz Game")
+window.geometry("500x500")
+window.mainloop()
