4c092335090cf596bcfafe5d685844910311f5b4
wsdula <wsdula@gmail.com>
2024-01-20 20:54:49 -0500

    reframed some portions

--- ui.py
+++ ui.py
@@ -8,7 +8,6 @@ import tkinter as tk
 # import utils
 
 
-
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
@@ -31,12 +30,13 @@ class App(tk.Tk):
         self.show_frame("StartPage")
 
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
+        """Show a frame for the given page name"""
         frame = self.frames[page_name]
         frame.tkraise()
 
     def close_out(self):
         self.destroy()
+
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -45,14 +45,23 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Welcome to the Quiz Game!", font=("Helvetica", 16))
         label.pack(side="top", fill="x", pady=20)
 
-        button1 = tk.Button(self, text="Start Game", command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="View Games", command=lambda: controller.show_frame("PageTwo"))
-        button3 = tk.Button(self, text="Edit Player Database", command=lambda: controller.show_frame("PageThree"))
+        button1 = tk.Button(
+            self, text="Start Game", command=lambda: controller.show_frame("PageOne")
+        )
+        button2 = tk.Button(
+            self, text="View Games", command=lambda: controller.show_frame("PageTwo")
+        )
+        button3 = tk.Button(
+            self,
+            text="Edit Player Database",
+            command=lambda: controller.show_frame("PageThree"),
+        )
         button4 = tk.Button(self, text="Exit", command=lambda: controller.close_out())
         button1.pack(pady=10)
         button2.pack(pady=10)
         button3.pack(pady=10)
         button4.pack(pady=10)
+
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
@@ -62,5 +71,9 @@ class PageOne(tk.Frame):
         # Add game logic here
         game_label = tk.Label(self, text="Game in progress!", font=("Helvetica", 16))
         game_label.pack(pady=20)
-        button = tk.Button(self, text="Go to the start page", command=lambda: controller.show_frame("StartPage"))
+        button = tk.Button(
+            self,
+            text="Go to the start page",
+            command=lambda: controller.show_frame("StartPage"),
+        )
         button.pack(pady=10)
