07032934e385f9576bbd4436b918040300402e00
wsdula <wsdula@gmail.com>
2024-01-29 16:06:26 -0500

    feat: made question, player, team classes extensible

--- utils.py
+++ utils.py
@@ -1,24 +1,30 @@
 # Questions have a prompt and an answer and a value
 class Question:
-    def __init__(self, prompt, answer, value=10):
+    def __init__(self, prompt, answer, value=10, **kwargs):
         self.id = None  # unique identifier for each Question in database
         self.prompt = prompt
         self.answer = answer
         self.value = value
+        for k,v in kwargs.items():
+            setattr(self, k, v)
 
 
 class Player:
     # TODO: Redefine this class to be more useful
-    def __init__(self, name: str, team: str):
+    def __init__(self, name: str, team: str, **kwargs):
         self.name = name
         self.team = team
+        for k,v in kwargs.items():
+            setattr(self, k, v)
 
 
 class Team:
     # TODO: Change to make use of Player class
-    def __init__(self, name: str, players: list[str]):
+    def __init__(self, name: str, players: list[str], **kwargs):
         self.name = name
         self.players = players
+        for k,v in kwargs.items():
+            setattr(self, k, v)
 
     def __str__(self):
         return f"Team: {self.name}"
@@ -31,6 +37,7 @@ def get_questions_from_file(filename: str) -> list[Question]:
     """Reads questions from a file and returns a list of Question objects"""
     questions = []
     with open(filename, "r") as f:
+        #FIXME: This is not the best way to read from a file
         for line in f:
             prompt, answer = line.split(";")
             questions.append(Question(prompt, answer))
