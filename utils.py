78aef4a765d51308a6ee0119eb04e6729cdecdd9
wsdula <wsdula@gmail.com>
2024-01-29 16:34:42 -0500

    feat: added scoring methods to players and team generics

--- utils.py
+++ utils.py
@@ -5,25 +5,26 @@ class Question:
         self.prompt = prompt
         self.answer = answer
         self.value = value
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
 
 class Player:
     # TODO: Redefine this class to be more useful
-    def __init__(self, name: str, team: str, **kwargs):
+    def __init__(self, name: str, score: int = 0, **kwargs):
         self.name = name
-        self.team = team
-        for k,v in kwargs.items():
+        self.score = score
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
 
 class Team:
     # TODO: Change to make use of Player class
-    def __init__(self, name: str, players: list[str], **kwargs):
+    def __init__(self, name: str, members: list[Player], score: int = 0, **kwargs):
         self.name = name
-        self.players = players
-        for k,v in kwargs.items():
+        self.members = members
+        self.score = score
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
     def __str__(self):
@@ -32,15 +33,21 @@ class Team:
     def __repr__(self):
         return f"Team: {self.name}"
 
+    def UpdateTeamScore(self):
+        self.score = sum(member.score for member in self.members)
+
 
 def get_questions_from_file(filename: str) -> list[Question]:
     """Reads questions from a file and returns a list of Question objects"""
     questions = []
     with open(filename, "r") as f:
-        #FIXME: This is not the best way to read from a file
+        # FIXME: This is not the best way to read from a file
         for line in f:
             prompt, answer = line.split(";")
             questions.append(Question(prompt, answer))
     return questions
 
 
+def create_player(name: str) -> Player:
+    """Creates a Player object"""
+    return Player(name)
