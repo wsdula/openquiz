584d485b23ed3973343b7768ebed4767030a53ed
wsdula <wsdula@gmail.com>
2024-02-27 16:38:33 -0500

    feat: added answer buttons for both teams

--- format/default/ui.py
+++ format/default/ui.py
@@ -46,7 +46,11 @@ class GamePage(tk.Frame):
         self.qFrame = tk.Frame(self, bg="blue")
         self.qVar.set("Question goes here")
         self.qText = tk.Label(
-            self.qFrame, textvariable=self.qVar, font=("Helvetica", 24)
+            self.qFrame,
+            textvariable=self.qVar,
+            font=("Helvetica", 24),
+            wraplength=1000,
+            justify="center",
         )
         self.qFrame.pack(pady=20)
         self.qText.pack(pady=20)
@@ -56,13 +60,16 @@ class GamePage(tk.Frame):
         self.scoreVar.set("Score goes here")
         self.scoreText = tk.Label(
             self.scoreFrame, textvariable=self.scoreVar, font=("Helvetica", 24)
-        )
-        self.correct_button = tk.Button(self.scoreFrame, text="Correct")
-        self.wrong_button = tk.Button(self.scoreFrame, text="Wrong")
+        ).grid(row=0, column=1)
+        self.left_correct_button = tk.Button(self.scoreFrame, text="Correct")
+        self.left_correct_button.grid(row=1, column=0, pady=10)
+        self.left_wrong_button = tk.Button(self.scoreFrame, text="Wrong")
+        self.left_wrong_button.grid(row=2, column=0)
+        self.right_correct_button = tk.Button(self.scoreFrame, text="Correct")
+        self.right_correct_button.grid(row=1, column=2, pady=10)
+        self.right_wrong_button = tk.Button(self.scoreFrame, text="Wrong")
+        self.right_wrong_button.grid(row=2, column=2)
         self.scoreFrame.pack(pady=20)
-        self.correct_button.pack(pady=10)
-        self.wrong_button.pack(pady=10)
-        self.scoreText.pack(pady=20)
 
     def gameLoop(self, game):
         while game.flag:
@@ -75,6 +82,24 @@ class GamePage(tk.Frame):
 
             self.endButton.configure(command=lambda: endGame())
             for question in game.rounds.questions:
+                self.left_correct_button.configure(
+                    command=lambda: [
+                        controls.correct_answer(game.teams[0], game.teams[0].members[0], question),
+                        okVar.set(1),
+                    ]
+                )
+                self.left_wrong_button.configure(
+                    command=lambda: [controls.wrong_answer(), okVar.set(1)]
+                )
+                self.right_correct_button.configure(
+                    command=lambda: [
+                        controls.correct_answer(game.teams[1], game.teams[1].members[0], question),
+                        okVar.set(1),
+                    ]
+                )
+                self.right_wrong_button.configure(
+                    command=lambda: [controls.wrong_answer(), okVar.set(1)]
+                )
                 self.qVar.set(question.prompt)
                 # alternate between teams
                 team = game.teams[game.rounds.questions.index(question) % team_count]
@@ -83,19 +108,16 @@ class GamePage(tk.Frame):
                     game.rounds.questions.index(question) % len(team.members)
                 ]
                 if game.rounds.questions.index(question) % team_count == 0:
-                    self.scoreVar.set(f"LEFT TEAM: {game.teams[0].score}  Right Team: {game.teams[1].score}")
+                    self.scoreVar.set(
+                        f"LEFT TEAM: {game.teams[0].score}  Right Team: {game.teams[1].score}"
+                    )
                 if game.rounds.questions.index(question) % team_count == 1:
-                    self.scoreVar.set(f"Left Team: {game.teams[0].score}  RIGHT TEAM: {game.teams[1].score}")
-                self.correct_button.configure(
-                    command=lambda: [
-                        controls.correct_answer(team, player, question),
-                        okVar.set(1),
-                    ]
-                )
-                self.wrong_button.configure(
-                    command=lambda: [controls.wrong_answer(), okVar.set(1)]
-                )
-                self.correct_button.wait_variable(okVar)
-                self.scoreVar.set(f"Left Team: {game.teams[0].score}  Right Team: {game.teams[1].score}")
+                    self.scoreVar.set(
+                        f"Left Team: {game.teams[0].score}  RIGHT TEAM: {game.teams[1].score}"
+                    )
+                self.left_wrong_button.wait_variable(okVar)
+            self.scoreVar.set(
+                f"Final Score \n Left Team: {game.teams[0].score}  Right Team: {game.teams[1].score}"
+            )
 
             game.flag = False
